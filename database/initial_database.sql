0cf20bc3f5f01fddbaf2e0c02be060e7c8b17216
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2024-06-10 15:17:51 -0300

    refactor: adiciona constraints 'auto_increment', 'unique' e 'not null' em colunas do banco de dados

--- database/initial_database.sql
+++ database/initial_database.sql
@@ -1,16 +1,16 @@
 /* Initial Version of the Database */
 
 CREATE TABLE User_Types (
-    user_type_id INTEGER PRIMARY KEY,
-    type_name VARCHAR(30)
+    user_type_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    type_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE Users (
-    user_id INTEGER PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     user_type_id INTEGER,
-    email VARCHAR(50),
-    first_name VARCHAR(30),
-    last_name VARCHAR(50),
+    email VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     created_at TIMESTAMP,
 
     FOREIGN KEY (user_type_id)
@@ -19,18 +19,18 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Pages (
-    page_id INTEGER PRIMARY KEY,
-    page_name VARCHAR(100),
+    page_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    page_name VARCHAR(100) UNIQUE NOT NULL,
     page_url VARCHAR(100),
     created_at TIMESTAMP
 );
 
 CREATE TABLE Comments (
-    comment_id INTEGER PRIMARY KEY,
+    comment_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     page_id INTEGER,
     user_id INTEGER,
-    content VARCHAR(1000),
-    is_approved BOOLEAN,
+    content VARCHAR(1000) NOT NULL,
+    is_approved BOOLEAN NOT NULL,
     created_at TIMESTAMP,
 
     FOREIGN KEY (page_id)

5759cacd5f88473c51fa27cb16cd41b027bece35
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2024-06-18 12:02:40 -0300

    feat: adiciona valores internos para o banco de dados e constraints 'not null' para colunas

--- database/initial_database.sql
+++ database/initial_database.sql
@@ -1,13 +1,19 @@
-/* Initial Version of the Database */
+DROP DATABASE IF EXISTS volta_ao_mundo;
+CREATE DATABASE volta_ao_mundo;
+USE volta_ao_mundo;
 
 CREATE TABLE User_Types (
-    user_type_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    user_type_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     type_name VARCHAR(30) NOT NULL
 );
 
+INSERT INTO User_Types (type_name) VALUES 
+('admin'),
+('user');
+
 CREATE TABLE Users (
-    user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    user_type_id INTEGER,
+    user_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    user_type_id INTEGER NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
@@ -19,15 +25,22 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Pages (
-    page_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    page_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     page_name VARCHAR(100) UNIQUE NOT NULL,
     created_at TIMESTAMP
 );
 
+INSERT INTO Pages (page_name) VALUES 
+('cities'),
+('culture'),
+('home'),
+('places'),
+('statistics');
+
 CREATE TABLE Comments (
-    comment_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    page_id INTEGER,
-    user_id INTEGER,
+    comment_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    page_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     content VARCHAR(1000) NOT NULL,
     is_approved BOOLEAN NOT NULL,
     created_at TIMESTAMP,

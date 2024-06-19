457958cb2a140022032ef815a09bc1b9d2d71c1f
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2024-06-18 21:31:00 -0300

    feat: estiliza pagina do perfil do usuario

--- views/pages/my-profile.php
+++ views/pages/my-profile.php
@@ -9,28 +9,34 @@ if ($_SESSION['user_type'] == 'admin') {
 require_once fullPath('views/components/header.php');
 require_once fullPath('backend/comments_queries.php');
 
+$user_comments_number = countCommentsByUserId($_SESSION['user_id']);
 $user_comments = getCommentsByUserId($_SESSION['user_id']);
 ?>
 
 <div class="container">
     <div class="d-flex justify-content-center py-3">
         <div class="w-50">
-            <div class="bg-white p-4 rounded-3 mb-3">
-                <p>Olá, 
-                    <span class="fs-2"><?= $_SESSION['user_first_name'] ?> <?= $_SESSION['user_last_name'] ?></span>
-                </p>
-
-                <p>Conta Criada em <?= $_SESSION['user_created_at'] ?></p>
+            <div class="mb-5">
+                <p class="fs-3 text-center">Minha Conta</p>
+                <div class="bg-white p-4 rounded-3 mb-3">
+                    <p class="mb-5">Olá,
+                        <span class="fs-2"><?= $_SESSION['user_first_name'] ?> <?= $_SESSION['user_last_name'] ?></span>
+                    </p>
+                    <div class="d-flex justify-content-between">
+                        <p>Conta Criada em <?= $_SESSION['user_created_at'] ?></p>
+                        <p><?= $user_comments_number ?> comentários realizados</p>
+                    </div>
+                </div>
             </div>
-            <div class="bg-white p-4 rounded-3">
-                <p class="fs-3">Meus Comentários</p>
+            <div class="mb-5">
+                <p class="fs-3 text-center">Meus Comentários</p>
                 <?php if ($user_comments) : ?>
                     <?php foreach ($user_comments as $comment) : ?>
                         <div class="py-2">
                             <div class="bg-white rounded-3 p-3">
                                 <div class="d-flex justify-content-between">
                                     <p><?= $comment['created_at'] ?></p>
-                                    <p class="fs-5 fw-bold">Comentário em <?= $comment['page_name'] ?></p>
+                                    <p class="fs-5 fw-bold">Pagina "<?= $comment['page_display_name'] ?>"</p>
                                 </div>
                                 <p><?= $comment['content'] ?></p>
                                 <?php if ($comment['is_approved']) : ?>
@@ -42,9 +48,15 @@ $user_comments = getCommentsByUserId($_SESSION['user_id']);
                         </div>
                     <?php endforeach ?>
                 <?php else : ?>
-                    <h6>Você ainda não possui comentários</h6>
+                    <div class="d-flex justify-content-center align-items-center bg-white py-5 rounded-4">
+                        <p class="fs-5">Você ainda não possui comentários</p>
+                    </div>
                 <?php endif; ?>
             </div>
         </div>
     </div>
 </div>
+
+<?php 
+    require_once fullPath('views/components/footer.html');
+?>

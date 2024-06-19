b5aa8b83f54087dc0990d5fa0490023764787c91
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2024-06-18 21:30:36 -0300

    feat: estiliza paginas de login e cadastro

--- views/pages/sign-up.php
+++ views/pages/sign-up.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -6,40 +6,98 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Cadastra-se</title>
+    <link rel="stylesheet" href="../../assets/css/pages.css">
 </head>
 
 <body>
-    <div class="container">
-        <div class="vh-100  d-flex justify-content-center align-items-center">
-            <div class="w-50 bg-secondary p-5 rounded-3">
-                <h1>Cadastre-se</h1>
-                <form action="../../backend/users_actions.php" method="post" id="login-form">
-                    <input type="hidden" name="action" value="sign_up">
-                    <div class="mb-3">
-                        <label for="txtFirstName" class="form-label">Nome</label>
-                        <input type="text" id="txtFirstName" name="first_name" class="form-control">
+    <main>
+        <section>
+            <div class="container">
+                <div class="vh-100  d-flex justify-content-center align-items-center">
+                    <div class="w-50 bg-secondary p-5 rounded-3">
+                        <h1>Cadastre-se</h1>
+                        <form action="../../backend/users_actions.php" method="post" id="login-form">
+                            <input type="hidden" name="action" value="sign_up">
+                            <div class="mb-3">
+                                <label for="txtFirstName" class="form-label">Nome</label>
+                                <input type="text" id="txtFirstName" name="first_name" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="txtLastName" class="form-label">Sobrenome</label>
+                                <input type="text" id="txtLastName" name="last_name" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="txtEmail" class="form-label">E-mail</label>
+                                <input type="email" id="txtEmail" name="email" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="txtPassword" class="form-label">Senha</label>
+                                <input type="password" id="txtPassword" name="password" class="form-control">
+                            </div>
+                            <input type="submit" value="Cadastrar" class="btn btn-success">
+                        </form>
+                        <?php if (isset($_GET['sign_up_status']) && $_GET['sign_up_status'] == 'already_registered') : ?>
+                            <p id="errorMessage">Usuário já cadastrado!</p>
+                        <?php endif; ?>
+                        <p>Já possui uma conta? <a href="./login.php">Entre</a></p>
                     </div>
-                    <div class="mb-3">
-                        <label for="txtLastName" class="form-label">Sobrenome</label>
-                        <input type="text" id="txtLastName" name="last_name" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="txtEmail" class="form-label">E-mail</label>
-                        <input type="email" id="txtEmail" name="email" class="form-control">
-                    </div>
-                    <div class="mb-3">
-                        <label for="txtPassword" class="form-label">Senha</label>
-                        <input type="password" id="txtPassword" name="password" class="form-control">
-                    </div>
-                    <input type="submit" value="Cadastrar" class="btn btn-success">
-                </form>
-                <?php if (isset($_GET['sign_up_status']) && $_GET['sign_up_status'] == 'already_registered') : ?>
-                    <p id="errorMessage">Usuário já cadastrado!</p>
-                <?php endif; ?>
-                <p>Já possui uma conta? <a href="./login.php">Entre</a></p>
+                </div>
             </div>
-        </div>
-    </div>
+        </section>
+    </main>
+</body>
+
+</html> -->
+
+<!DOCTYPE html> 
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../assets/css/pages.css">
+</head>
+
+<body>
+    <main>
+        <section>
+            <div class="container">
+                <div class="vh-100  d-flex justify-content-center align-items-center">
+                    <div class="w-50 bg-white p-5 rounded-3">
+                    <h1 class="mb-3">Cadastre-se</h1>
+                        <form action="../../backend/users_actions.php" method="post" id="login-form">
+                            <input type="hidden" name="action" value="sign_up">
+                            <div class="mb-3">
+                                <label for="txtFirstName" class="form-label">Nome</label>
+                                <input type="text" id="txtFirstName" name="first_name" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="txtLastName" class="form-label">Sobrenome</label>
+                                <input type="text" id="txtLastName" name="last_name" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="txtEmail" class="form-label">E-mail</label>
+                                <input type="email" id="txtEmail" name="email" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="txtPassword" class="form-label">Senha</label>
+                                <input type="password" id="txtPassword" name="password" class="form-control">
+                            </div>
+                            <input type="submit" value="Cadastrar" class="btn btn-primary">
+                        </form>
+                        <?php if (isset($_GET['sign_up_status']) && $_GET['sign_up_status'] == 'already_registered') : ?>
+                            <p id="errorMessage">Usuário já cadastrado!</p>
+                        <?php endif; ?>
+                        <div class="d-flex justify-content-end">
+                            <p>Já possui uma conta? <a href="./login.php" class="text-primary">Entre</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </body>
 
 </html>

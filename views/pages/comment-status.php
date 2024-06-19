a51500ede3184fbfb9c351c75523eb0b638966d0
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2024-06-18 21:31:34 -0300

    feat: estiliza pagina de mensagem do comentario utilizado

--- views/pages/comment-status.php
+++ views/pages/comment-status.php
@@ -1,5 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/volta-ao-mundo-australia/helpers/full-path.php';
+require_once fullPath('backend/session_authentication.php');
 require_once fullPath('views/components/header.php');
 require_once fullPath('backend/pages_queries.php');
 
@@ -7,16 +8,22 @@ $current_page_id = getPageIdByName('statistics');
 ?>
 
 <div class="container">
-    <?php if ($_GET['comment_status'] == 'success') : ?>
-        <div>
-            <h3>Obrigado pelo comentário!</h3>
-            <p>Assim que for aprovado por um administrador, ele será exibido no site!</p>
+    <div class="mt-5 d-flex  justify-content-center align-items-center">
+        <div class="w-50 bg-white p-5 rounded-4">
+            <?php if ($_GET['comment_status'] == 'success') : ?>
+                <div>
+                    <h2 class="text-center">Obrigado pelo comentário!</h2>
+                    <p class="text-center mt-3"> Assim que for aprovado por um administrador, ele será exibido no site!</p>
+                </div>
+            <?php elseif ($_GET['comment_status'] == 'error') :  ?>
+                <div>
+                    <h2 class="text-center">Ops, ocorreu algo de errado...</h2>
+                    <p class="text-center mt-3">Houve um erro no envio do comentário, por favor tente novamente mais tarde</p>
+                </div>
+            <?php endif ?>
+            <div class="d-flex justify-content-end pt-5">
+                <a href="./home.php" class="btn btn-primary">Voltar para a Página Inicial</a>
+            </div>
         </div>
-    <?php elseif ($_GET['comment_status'] == 'error') :  ?>
-        <div>
-            <h3>Ops, ocorreu algo de errado...</h3>
-            <p>Houve um erro no envio do comentário, por favor tente novamente mais tarde</p>
-        </div>
-    <?php endif ?>
-    <a href="./home.php">Voltar para a Página Inicial</a>
+    </div>
 </div>
